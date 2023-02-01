ae42fb3ab654e1c478da4ba23e29739f7f429aba
Pavel G <pavel@Pavels-MacBook-Pro.local>
2023-02-01 15:45:16 -0500

    add UsersController

--- API/Program.cs
+++ API/Program.cs
@@ -10,9 +10,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(opt =>
-{
-  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+  {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+  }
+);
 
 var app = builder.Build();
 
